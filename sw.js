10f6185483ca83608dfcdef596012354e69d214b
NoahSchuldt <noahglosan@icloud.com>
2026-05-13 08:17:57 +0200

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,23 +1,29 @@
-const CACHE_NAME = 'formula-practice-v6.1';
+const CACHE_NAME = "formula-practice-v6.3";
 
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/questions.json',
-  '/style.css'
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./questions.json"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
+
   event.waitUntil(
+
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
+
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
+
   event.respondWith(
+
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+
   );
 });
